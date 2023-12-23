62f76f79129021a4a4839996a7cc1e4078f5d26c
WebSavva <websavvap@gmail.com>
2023-12-23 23:36:24 +0300

    fix: client and server lint configurations were fixed finally

--- app/client/.eslintrc.cjs
+++ app/client/.eslintrc.cjs
@@ -6,20 +6,21 @@ module.exports = {
   },
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
 
-  plugins: [
-    '@typescript-eslint',
-  ],
-  
+  plugins: ['@typescript-eslint'],
+
   parser: 'vue-eslint-parser',
 
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.vue'],
   },
 
   rules: {
     'vue/no-unused-vars': 'error',
     'no-unused-vars': 'warn',
-    'no-undef': 'off', 
+    'no-undef': 'off',
   },
 };
--- app/server/.eslintrc.js
+++ app/server/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
 
   extends: [
